47f7b13a5383c51bf8979298846ec2b76fb3e589
Andrzej Kucharski <andrzej.kucharski@piksel.com>
2018-10-03 14:29:42 +0100

    Adding app module

---
+++ modules/app/manifests/init.pp
@@ -0,0 +1,47 @@
+class app {
+	user { 
+		'appuser':
+			ensure	=> 'present',
+			gid	=> '10100',
+			home	=> '/opt/appdir',
+			shell	=> '/bin/false',
+			uid	=> '10100';
+	}
+
+	group {
+	    'appgroup':
+			ensure	=> present,
+			gid	=> 10100;
+	    }
+
+	file {
+		'/opt/appdir':
+			ensure	=> directory,
+			owner	=> 'appuser',
+			group	=> 'appgroup',
+			mode	=> 750;
+	}
+
+	file {
+		'/opt/appdir/log':
+			ensure	=> 'directory',
+			owner	=> 'appuser',
+			group	=> 'appgroup',
+			mode	=> 750;
+	}
+
+	file {
+		'/etc/logrotate.d/app-log-rotation.conf':
+			ensure	=> 'file',
+			owner	=> root,
+			group	=> root,
+			source	=> [ 'puppet:///modules/app/app-log-rotation.conf' ];
+	}
+
+	cron {
+		'app_logrotate':
+			command => '/usr/sbin/logrotate /etc/logrotate.d/app-log-rotation.conf',
+			user	=> root,
+			minute	=> '*/30',
+			hour	=> '*';
+}
